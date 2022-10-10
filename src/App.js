ecb1053f10071c3e27a63dac53f3421f310f755b
praneethbairy <bairypraneeth@gmail.com>
2022-10-10 10:33:30 +0000

    Multilingual-Greetings

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+
+import './App.css'
+
+const languageGreetingsList = [
+  {
+    id: 'bfdf40eb-eec9-4a66-a493-752fe689f0d0',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+    buttonText: 'English',
+    imageAltText: 'english',
+  },
+  {
+    id: '0ceda891-2a0c-49e2-8c62-68e78180bac6',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/tamil-greetings-img.png',
+    buttonText: 'Tamil',
+    imageAltText: 'tamil',
+  },
+  {
+    id: '89537778-7a46-4c58-988c-0adc931d087c',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/telugu-greetings-img.png',
+    buttonText: 'Telugu',
+    imageAltText: 'telugu',
+  },
+]
+
+// Replace your code here
+class App extends Component {
+  state = {
+    initialState: languageGreetingsList[0].imageUrl,
+    imagAltText: languageGreetingsList[0].imageAltText,
+  }
+
+  render() {
+    const {initialState, imagAltText} = this.state
+    return (
+      <div className="app-container">
+        <h1 className="main-heading">Multilingual Greetings</h1>
+        <ul className="buttons-container">
+          {languageGreetingsList.map(each => (
+            <li key={each.id}>
+              <button
+                className="each-button"
+                type="button"
+                onClick={this.onClickChangeStatus}
+              >
+                {each.buttonText}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <img className="description" src={initialState} alt={imagAltText} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
